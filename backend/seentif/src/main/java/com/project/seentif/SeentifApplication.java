bd843f9c8d78d5642334da3159ab17e62ab97f56
verisign_90 <verisign_90@naver.com>
2025-03-25 17:27:29 +0900

    [CHORE] Vite React / Spring Boot Project init

---
+++ backend/seentif/src/main/java/com/project/seentif/SeentifApplication.java
@@ -0,0 +1,13 @@
+package com.project.seentif;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeentifApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeentifApplication.class, args);
+	}
+
+}
