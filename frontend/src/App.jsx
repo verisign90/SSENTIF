cd71e7f7d0f8d99da8201323b3c7e0f8231e0bdc
verisign_90 <verisign_90@naver.com>
2025-03-26 21:27:29 +0900

    [CHORE] Netlify 배포 테스트

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,14 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Test from "./Test";
 
 function App() {
-  const [data, setData] = useState("");
-
-  useEffect(() => {
-    axios
-      .get("/api/data")
-      .then((response) => {
-        setData(response.data);
-      })
-      .catch((error) => {
-        console.error("axios fetching error: ", error);
-      });
-  }, []);
-
   return (
     <>
-      <h1>Hello Vite React</h1>
-      <p>스프링부트에서 보낸 메시지 : {data}</p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Test />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ frontend/src/Test.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+function Test() {
+  const [data, setData] = useState("");
+
+  useEffect(() => {
+    axios
+      .get("/api/data")
+      .then((response) => {
+        setData(response.data);
+      })
+      .catch((error) => {
+        console.error("axios fetching error: ", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>Hello Vite React</h1>
+      <p>스프링부트에서 보낸 메시지 : {data}</p>
+    </>
+  );
+}
+
+export default Test;
